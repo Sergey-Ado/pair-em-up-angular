e76482a155371d0aec02f5218f714215d87f4b32
sergey-ado <adonisiy2@gmail.com>
2025-11-21 20:48:26 +0400

    init: start pair-em-up-angular project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'pair-em-up-angular';
