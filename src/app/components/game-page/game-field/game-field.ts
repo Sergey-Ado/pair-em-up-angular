76ef3ba1c8aea44e07f2826b7231300fa2525d4a
sergey-ado <adonisiy2@gmail.com>
2025-11-23 20:38:56 +0400

    feat: add gameField

---
+++ src/app/components/game-page/game-field/game-field.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-field',
+  imports: [],
+  templateUrl: './game-field.html',
+  styleUrl: './game-field.css',
+})
+export class GameField {}
--- src/app/components/game-page/game-page.ts
+++ src/app/components/game-page/game-page.ts
@@ -3,10 +3,11 @@ import { ModeInfo } from './mode-info/mode-info';
 import { TimeScore } from './time-score/time-score';
 import { ControlTools } from './control-tools/control-tools';
 import { AssistTools } from './assist-tools/assist-tools';
+import { GameField } from './game-field/game-field';
 
 @Component({
   selector: 'app-game-page',
-  imports: [ModeInfo, TimeScore, ControlTools, AssistTools],
+  imports: [ModeInfo, TimeScore, ControlTools, AssistTools, GameField],
   templateUrl: './game-page.html',
   styleUrl: './game-page.css',
 })
