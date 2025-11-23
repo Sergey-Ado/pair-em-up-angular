758a42c96057d0a14627bfd59c3824a22259ee1b
sergey-ado <adonisiy2@gmail.com>
2025-11-23 19:28:48 +0400

    feat: add controlTools

---
+++ src/app/components/game-page/control-tools/control-tools.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-control-tools',
+  imports: [],
+  templateUrl: './control-tools.html',
+  styleUrl: './control-tools.css',
+})
+export class ControlTools {}
--- src/app/components/game-page/game-page.ts
+++ src/app/components/game-page/game-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ModeInfo } from './mode-info/mode-info';
 import { TimeScore } from './time-score/time-score';
+import { ControlTools } from './control-tools/control-tools';
 
 @Component({
   selector: 'app-game-page',
-  imports: [ModeInfo, TimeScore],
+  imports: [ModeInfo, TimeScore, ControlTools],
   templateUrl: './game-page.html',
   styleUrl: './game-page.css',
 })
