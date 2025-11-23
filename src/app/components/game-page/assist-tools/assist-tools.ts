eff6254b42681b70ea7e9af9081f87299e7fb812
sergey-ado <adonisiy2@gmail.com>
2025-11-23 20:23:41 +0400

    feat: add assistTools

---
+++ src/app/components/game-page/assist-tools/assist-tools.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assist-tools',
+  imports: [],
+  templateUrl: './assist-tools.html',
+  styleUrl: './assist-tools.css',
+})
+export class AssistTools {}
--- src/app/components/game-page/game-page.ts
+++ src/app/components/game-page/game-page.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { ModeInfo } from './mode-info/mode-info';
 import { TimeScore } from './time-score/time-score';
 import { ControlTools } from './control-tools/control-tools';
+import { AssistTools } from './assist-tools/assist-tools';
 
 @Component({
   selector: 'app-game-page',
-  imports: [ModeInfo, TimeScore, ControlTools],
+  imports: [ModeInfo, TimeScore, ControlTools, AssistTools],
   templateUrl: './game-page.html',
   styleUrl: './game-page.css',
 })
