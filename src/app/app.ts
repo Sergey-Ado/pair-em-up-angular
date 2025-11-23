18c574a232e2718c4283060d0f19f1526e3ab8d5
sergey-ado <adonisiy2@gmail.com>
2025-11-23 10:02:53 +0400

    feat: add header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { StartPage } from './components/start-page/start-page';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [StartPage],
+  imports: [StartPage, Header],
 })
 export class App {
   protected title = 'pair-em-up-angular';
