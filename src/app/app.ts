572c2f127c506f9e392eb337fb9a7b9d0aad118e
sergey-ado <adonisiy2@gmail.com>
2025-12-11 23:41:30 +0400

    feat: add start gameSave during reload application

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { StartPage } from './components/start-page/start-page';
 import { Header } from './components/header/header';
 import { Store } from '../store/store';
 import { GamePage } from './components/game-page/game-page';
 import { Modal } from './components/modals/modal/modal';
 import { ResultsModal } from './components/modals/results-modal/results-modal';
+import { StorageService } from './services/storage-service';
 
 @Component({
   selector: 'app-root',
@@ -14,4 +15,10 @@ import { ResultsModal } from './components/modals/results-modal/results-modal';
 })
 export class App {
   protected readonly store = inject(Store);
+  private storageService = inject(StorageService);
+
+  @HostListener('window:beforeunload')
+  public onBeforeUnload(): void {
+    this.storageService.saveGame();
+  }
 }
